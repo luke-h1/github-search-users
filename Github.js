a026c66f4ed2a7c94d98e2007c08b1e7f6d3bb39
Luke H <55030296+luke-h1@users.noreply.github.com>
2020-08-19 19:23:35 +0100

    sanitise

--- Github.js
+++ Github.js
@@ -8,10 +8,10 @@ class Github {
 
     async getUser(user) {
         const profileResponse = await fetch(
-            `https://api.github.com/users/${user}?client_ID={CLIENT_ID}&client_secret={CLIENT_SECRET}`
+            `https://api.github.com/users/${user}?client_ID={YOUR_CLIENT_ID}&client_secret={YOUR_CLIENT_SECRET}`
         );
         const repoResponse = await fetch(
-            `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_ID=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+            `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_ID={YOUR_CLIENT_ID}&client_secret={YOUR_CLIENT_SECRET}`
         );
         const profileData = await profileResponse.json();
         const repos = await repoResponse.json();
